5f6dc70f3d5bb235a3f5d572851144e359d6e206
Dark Learn <code.talha.dev1@gmail.com>
2026-07-11 04:26:05 -0700

    Print avg marks take 3 number

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+    // Print Average Marks Taking 3 inputs...
+    int n1, n2, n3;
+    cout<<"Enter marks1: ";
+    cin>>n1;
+    cout<<"Enter marks2: ";
+    cin>>n2;
+    cout<<"Enter marks3: ";
+    cin>>n3;
+    float avg = (n1 + n2 + n3)/3;
+    cout<<"Average marks: " << avg << "\n";
+    return 0;
+}
