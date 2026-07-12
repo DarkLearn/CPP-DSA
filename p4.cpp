689fc298bbdea6329a81cdb3a7586c97ebb70b6f
Dark Learn <code.talha.dev1@gmail.com>
2026-07-11 18:27:57 -0700

    Print Square of Area

---
+++ p4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float l;
+    cout <<"Enter Length: ";
+    cin >> l;
+
+    float area = l * l;
+    cout <<"Square of Area: " << area << endl;
+    return 0;
+}
