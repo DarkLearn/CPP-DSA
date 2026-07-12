f78b4bfdf23a554d1093a2179d320236f8ca1b1b
Dark Learn <code.talha.dev1@gmail.com>
2026-07-11 18:43:35 -0700

    Print Area of Circle

---
+++ p7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float r;
+    cout <<"Enter radius: ";
+    cin >> r;
+
+    float area = 3.14159 * r * r;
+    cout << "Area of circle: " << area << endl;
+    return 0;
+}
